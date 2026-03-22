b955ed1cbf71cab23583fbd27247a65bc6eba9c0
NavyasriAnumula <navyasrianumula@creamcollar.com>
2026-03-23 00:42:41 +0530

    Update import for ChatOpenAI from langchain_openai

--- TTC_MERGING_COMPLIANCES.py
+++ TTC_MERGING_COMPLIANCES.py
@@ -7,7 +7,7 @@ import sys
 import time
 import asyncio
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
 from langchain.chains import LLMChain
 # ============================================================================
